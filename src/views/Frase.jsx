67ca925d01430727cab9127b36fa555ac0eb613b
Aixa Filsinger <macafil99@gmail.com>
2025-09-17 22:57:01 -0300

    centre la frase y las imagenes

--- src/views/Frase.jsx
+++ src/views/Frase.jsx
@@ -3,7 +3,7 @@ import adorno from '../assets/adorno.png'
 
 const Frase = () => {
     return (
-        <section className='container text-center p-5'>
+        <section className='container text-center p-5 d-flex flex-column aling-items-center'>
             <img src={adorno} alt="" width={300}/>
             <p className='frase1 my-3'>~ Mi vida es como un cuento de hadas, llena de alegrías y sorpresas inesperadas, 
                  y ahora me presento como una princesa para invitarte a mis quince primaveras ~</p>
